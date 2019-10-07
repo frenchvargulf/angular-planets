c843d150f18737632530ca713897f67b095ac866
frenchvargulf <marta.s.czarnecka@gmail.com>
2019-10-07 10:53:28 +0200

    Add routing module, add to main app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PlanetsComponent } from "./planets/planets.component";
+import { PlanetDetailComponent } from "./planet-detail/planet-detail.component";
 
-
+const routes: Routes = [
+  { path: "planets", component: PlanetsComponent },
+  { path: "", redirectTo: "/planets", pathMatch: "full" },
+  { path: "planet/:name", component: PlanetDetailComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
